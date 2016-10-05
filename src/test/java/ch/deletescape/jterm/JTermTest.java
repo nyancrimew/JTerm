a99f4c6d284953854bb28b58693856f3bde40094
deletescape <deletescape@outlook.com>
2016-10-05 14:58:36 +0200

    correctly setup gradle and add jacoco test coverage

---
+++ src/test/java/ch/deletescape/jterm/JTermTest.java
@@ -0,0 +1,14 @@
+package ch.deletescape.jterm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class JTermTest {
+
+  @Test
+  public void test() {
+    assertTrue(true);
+  }
+
+}

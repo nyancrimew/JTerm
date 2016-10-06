6c9896df6898faa7b6c56354da2d474291d50553
deletescape <deletescape@outlook.com>
2016-10-06 08:57:28 +0200

    Tests CommandUtils#evaluateCommand and CommandUtils#parseInlineCommands

--- src/test/java/ch/deletescape/jterm/CommandUtilsTest.java
+++ src/test/java/ch/deletescape/jterm/CommandUtilsTest.java
@@ -3,6 +3,8 @@ package ch.deletescape.jterm;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class CommandUtilsTest {
@@ -17,4 +19,14 @@ public class CommandUtilsTest {
     assertThat(CommandUtils.getArgs("echo   Test "), is("Test"));
   }
 
+  @Test
+  public void testEvaluateCommand() throws IOException {
+    CommandUtils.initializeEnv();
+    assertThat(CommandUtils.evaluateCommand("echo Test"), is("Test"));
+  }
+  @Test
+  public void testInlineCommandParsing() throws IOException  {
+    CommandUtils.initializeEnv();
+    assertThat(CommandUtils.parseInlineCommands("Test ${echo tst}"),is("Test tst"));
+  }
 }

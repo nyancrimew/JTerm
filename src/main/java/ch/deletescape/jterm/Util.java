4807bc45d8611e6f99751f9527d7a8a62930a2f1
deletescape <deletescape@outlook.com>
2016-10-24 13:03:28 +0200

    add javadoc to Util

--- src/main/java/ch/deletescape/jterm/Util.java
+++ src/main/java/ch/deletescape/jterm/Util.java
@@ -5,10 +5,27 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Util {
+  /**
+   * Replaces the tilde (~) with the home directory in a string
+   * 
+   * @param cmd
+   *          The path to parse
+   * @return The new String
+   */
   public static String makePathString(String cmd) {
     return cmd.replace("~", JTerm.getHome());
   }
 
+  /**
+   * Writes the content of input into output
+   * 
+   * @param input
+   *          The {@link InputStream} to be copied to
+   * @param output
+   *          The target {@link OutputStream} to copy to
+   * @return The content of input
+   * @throws IOException
+   */
   public static String copyStream(InputStream input, OutputStream output) throws IOException {
     StringBuilder sb = new StringBuilder();
     byte[] buffer = new byte[1024];

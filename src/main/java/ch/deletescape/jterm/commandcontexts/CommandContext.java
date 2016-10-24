139823eb5b71c9e8c8042150eaf05c82c9889f88
deletescape <deletescape@outlook.com>
2016-10-24 12:57:18 +0200

    adds javadoc for CommandContext

--- src/main/java/ch/deletescape/jterm/commandcontexts/CommandContext.java
+++ src/main/java/ch/deletescape/jterm/commandcontexts/CommandContext.java
@@ -1,10 +1,24 @@
 package ch.deletescape.jterm.commandcontexts;
 
+import ch.deletescape.jterm.CommandUtils;
+
+/**
+ * Abstract class to be extended by command contexts to provide the required api
+ * 
+ * @author deletescape
+ */
 public abstract class CommandContext {
+  /**
+   * Public constructor, calls {@link #init()} to initialize the context
+   */
   public CommandContext() {
     init();
   }
 
+  /**
+   * Initialization method, should be used to register commands by calls to
+   * {@link CommandUtils#addListener(String, ch.deletescape.jterm.CommandExecutor)}
+   */
   protected abstract void init();
 
 }

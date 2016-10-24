3e1404a34d35199de135b62b0d9ed870e61e52d3
deletescape <deletescape@outlook.com>
2016-10-24 13:42:51 +0200

    add javadoc to CommandExecutor

--- src/main/java/ch/deletescape/jterm/CommandExecutor.java
+++ src/main/java/ch/deletescape/jterm/CommandExecutor.java
@@ -1,6 +1,20 @@
 package ch.deletescape.jterm;
 
+/**
+ * FunctionalInterface used for the action of a command
+ * 
+ * @author deletescape
+ */
 @FunctionalInterface
 public interface CommandExecutor {
-    Object exec(String args) throws Exception;
+  /**
+   * This method should contain the logic of a command, or a call to this logic
+   * 
+   * @param args
+   *          The arguments for the command
+   * @return The return value of the command
+   * @throws Exception
+   *           To enable exception throwing from commands
+   */
+  Object exec(String args) throws Exception;
 }

886cf69672952e650da5ee61b5ec12b6f3920b31
deletescape <deletescape@outlook.com>
2016-10-26 08:09:45 +0200

    add test for UserProperties

---
+++ src/test/java/ch/deletescape/jterm/config/UserPropertiesTest.java
@@ -0,0 +1,38 @@
+package ch.deletescape.jterm.config;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class UserPropertiesTest {
+
+  @Test
+  public void setProperty() {
+    String key = "test" + Math.random();
+    UserProperties.setProperty(key, "works");
+    assertThat(UserProperties.getProperty(key), is("works"));
+  }
+
+  @Test
+  public void getBooleanPropertyIsTrue() {
+    String key = "test" + Math.random();
+    UserProperties.setProperty(key, "true");
+    assertThat(UserProperties.getBoolean(key), is(true));
+    key = "test" + Math.random();
+    UserProperties.setProperty(key, "1");
+    assertThat(UserProperties.getBoolean(key), is(true));
+  }
+  @Test
+  public void getBooleanPropertyIsFalse() {
+    String key = "test" + Math.random();
+    UserProperties.setProperty(key, "false");
+    assertThat(UserProperties.getBoolean(key), is(false));
+    key = "test" + Math.random();
+    UserProperties.setProperty(key, "0");
+    assertThat(UserProperties.getBoolean(key), is(false));
+    key = "test" + Math.random();
+    UserProperties.setProperty(key, "ab");
+    assertThat(UserProperties.getBoolean(key), is(false));
+  }
+}

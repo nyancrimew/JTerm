b3e084114cf92ef2ee414dcc2f06eb7f606ea01b
deletescape <deletescape@outlook.com>
2016-10-26 10:16:23 +0200

    refactor Directories

--- src/main/java/ch/deletescape/jterm/commandcontexts/Directories.java
+++ src/main/java/ch/deletescape/jterm/commandcontexts/Directories.java
@@ -1,16 +1,21 @@
 package ch.deletescape.jterm.commandcontexts;
 
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import ch.deletescape.jterm.CommandUtils;
 import ch.deletescape.jterm.JTerm;
 import ch.deletescape.jterm.Util;
 import ch.deletescape.jterm.config.Resources;
 import ch.deletescape.jterm.io.Printer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.nio.file.*;
-import java.util.stream.Stream;
 
 public class Directories extends CommandContext {
   private static final Logger LOGGER = LogManager.getLogger();
@@ -41,18 +46,17 @@ public class Directories extends CommandContext {
 
   private String ls(String cmd) throws IOException {
     String command = CommandUtils.parseInlineCommands(cmd);
-    StringBuilder out = new StringBuilder();
     Path path = JTerm.getCurrPath().resolve(Util.makePathString(command));
     try (Stream<Path> stream = Files.list(path)) {
+      StringBuilder out = new StringBuilder();
       stream.forEach(p -> out.append(p.getFileName().toString() + "\n"));
+      return Printer.out.println(out);
     } catch (NoSuchFileException e) {
-      Printer.err.println(Resources.getString("PathNotFound"), path);
       LOGGER.error(e.toString(), e);
+      return Printer.err.println(Resources.getString("PathNotFound"), path);
     } catch (NotDirectoryException e) {
-      out.append(path);
-      out.append('\n');
+      return Printer.out.println(path);
     }
-    return Printer.out.println(out);
   }
 
   private boolean mkdir(String cmd) throws IOException {

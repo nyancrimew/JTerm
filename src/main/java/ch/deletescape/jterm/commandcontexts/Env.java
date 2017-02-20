f42258a2d635146eac41c4e8cfa44784201371e1
deletescape <deletescape@outlook.com>
2017-02-20 10:22:37 +0100

    Add the exec command

--- src/main/java/ch/deletescape/jterm/commandcontexts/Env.java
+++ src/main/java/ch/deletescape/jterm/commandcontexts/Env.java
@@ -1,12 +1,17 @@
 package ch.deletescape.jterm.commandcontexts;
 
+import java.io.IOException;
+
 import ch.deletescape.jterm.CommandUtils;
 import ch.deletescape.jterm.JTerm;
+import ch.deletescape.jterm.Util;
 import ch.deletescape.jterm.config.Resources;
 import ch.deletescape.jterm.config.UserProperties;
 import ch.deletescape.jterm.io.Printer;
 
 public class Env extends CommandContext {
+  private String prefix;
+  private String path;
 
   @Override
   protected void init() {
@@ -17,6 +22,7 @@ public class Env extends CommandContext {
     CommandUtils.addListener("mute", o -> mute());
     CommandUtils.addListener("setProp", this::setProp);
     CommandUtils.addListener("getProp", this::getProp);
+    CommandUtils.addListener("exec", this::exec);
   }
 
   private String getEnv(String cmd) {
@@ -86,6 +92,28 @@ public class Env extends CommandContext {
     StringBuilder sb = new StringBuilder();
     UserProperties.getProperties().forEach((o1, o2) -> sb.append(o1 + " = " + o2 + "\n"));
     return Printer.out.println(sb.toString());
+  }
+
+  String exec(String cmd) throws IOException {
+    String[] path = { getPath() };
+    String command = getPrefix() + CommandUtils.parseInlineCommands(cmd);
+    Process p = Runtime.getRuntime().exec(command, path, JTerm.getCurrPath().toFile());
+    return Util.copyStream(p.getInputStream(), Printer.out.getPrintStream());
+  }
+
+  private String getPrefix() {
+    if (prefix == null) {
+      boolean isWindows = System.getProperty("os.name").startsWith("Windows");
+      prefix = isWindows ? "cmd /c set PATH=" + getPath() + "&&" : "";
+    }
+    return prefix;
+  }
+
+  private String getPath() {
+    if (path == null) {
+      path = System.getenv("PATH");
+    }
+    return path;
   }
 
   boolean mute() {
